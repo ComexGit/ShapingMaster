c99e99205dfde5e83c97497fb3388bcd85a269fa
ComexGit <comexie@163.com>
2017-09-04 10:24:29 +0800

    create the project

---
+++ 塑形大师/塑形大师/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  塑形大师
+//
+//  Created by yuqian on 2017/9/4.
+//  Copyright © 2017年 com.tenyears. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
